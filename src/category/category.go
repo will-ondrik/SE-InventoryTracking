7fabbe829172ec1c5d21d054b8a96db9e823fdf2
will-ondrik <wondrik@my.bcit.ca>
2025-11-03 19:16:36 -0800

    Category and subcategory structs init.

--- src/category/category.go
+++ src/category/category.go
@@ -1,9 +1,13 @@
 package category
 
-type Category struct {
+import "sandbox/straightedge/qr/SE-InventoryTracking/src/tool"
 
+type Category struct {
+	Name string
+	SubCategory SubCategory
 }
 
 type SubCategory struct {
-	
+	Name string
+	Tools []tool.Tool
 }
