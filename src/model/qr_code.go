36414f785ae722f3313c80c36e6fe1b9809df3c6
will-ondrik <wondrik@my.bcit.ca>
2025-11-05 05:57:06 -0800

    Updated structs.

--- src/model/qr_code.go
+++ src/model/qr_code.go
@@ -1,5 +1,39 @@
 package model
 
-type QrCode struct {
-	
+import (
+	"image"
+	"image/color"
+
+	"github.com/skip2/go-qrcode"
+	"golang.org/x/image/font"
+)
+
+// Recovery Level
+// Low, Medium, and High
+// Low -- Damaged code less likely to scan -- increases with level
+	type QrData struct {
+	Content string
+	RecoveryLevel qrcode.RecoveryLevel 
+	Size int
 }
+
+type QrCanvas struct {
+	Img *image.RGBA
+	Label string
+	Face font.Face
+	Col color.Color
+	Width int
+	BaselineY int
+}
+
+type QrCode struct {}
+
+type QrSize int 
+type LabelRatio float64
+
+const (
+	QrSmall QrSize = 256
+	QrMedium QrSize = 384
+	QrLarge QrSize = 512
+	LabelPad LabelRatio = 0.17
+)
