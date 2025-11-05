707fac588ff62349e579c0521c3153af0c17db03
will-ondrik <wondrik@my.bcit.ca>
2025-11-05 05:56:39 -0800

    Updated struct json tags.

--- src/model/tool.go
+++ src/model/tool.go
@@ -5,13 +5,14 @@ import (
 )
 
 type Tool struct {
-	Id string
-	Name string
-	IsAvailable bool
-	LastServiced time.Time
-	AssignedTo User
-	Condition string
-	QrCode QrCode
-	Job Job
+	Id string	`json:"id,omitempty"`
+	Name string	`json:"name"`
+	SubCategoryId SubCategory `json:"subCategoryId"`
+	IsAvailable bool `json:"isAvailable"`
+	LastServiced time.Time `json:"lastServiced"`
+	AssignedTo User `json:"assignedTo"`
+	Condition string `json:"condition"`
+	QrCode QrCode `json:"qrCode,omitempty"`
+	Job Job `json:"job"`
 }
 
